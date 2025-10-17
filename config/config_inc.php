fd4fade579b39dd2fb89f5aba74ff03e4e901f45
admin00 <admin00@VMLINUX03.vega.local>
2025-10-17 12:26:49 +0200

    Modificato il numero massimo di eventi in un'ora da 10 a 100

--- config/config_inc.php
+++ config/config_inc.php
@@ -20,6 +20,7 @@ $g_crypto_master_salt       = '7hTkuF6jZsN+AWsjhk/DVg5deGjG0aCT1ioM6VLDaJE=';
 $g_reauthentication_expiry  = 60*60*24;
 $g_bug_pending_status       = PENDING;
 $g_public_config_names[]    = 'bug_pending_status';
+$g_antispam_max_event_count = 100;
 
 $g_log_level                = LOG_EMAIL | LOG_EMAIL_RECIPIENT;
 
